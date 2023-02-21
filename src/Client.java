ade536e0a22c8cbff05c72067d19c96c9dea7700
Elfergouch Mohamed <elfergouchmohamed@gmail.com>
2023-02-21 01:48:21 +0100

    Ajout de la fichier README

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,4 @@
-public class Client extends Personne{
+public abstract class Client extends Personne{
     private int Id_client;
 
     public int getId_client() {
@@ -9,7 +9,12 @@ public class Client extends Personne{
         Id_client = id_client;
     }
 
-    public Client(int Id_cleint){
+    public Client(int Id_cleint, String Email, String MotDePasse, int DateNaissance, int Telephone, String Adresse){
+        super(Email,MotDePasse,DateNaissance,Telephone,Adresse);
         this.Id_client = Id_cleint;
+    }
+
+    public void chercher() {
+        System.out.println("Je suis Client");
     }
 }
